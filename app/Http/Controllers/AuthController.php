31e75df63942afb84cc1559b0246d04b0b736985
filbab <filbab@wp.pl>
2024-06-20 10:16:43 +0200

    update AuthController

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,16 +3,30 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Services\AuthService;
 use Symfony\Component\HttpFoundation\JsonResponse;
 
 class AuthController extends Controller
 {
+    public function __construct(
+        private AuthService $authService
+    ) {}
+
     public function login(Request $request): JsonResponse
     {
-        // TODO
-
-        return response()->json([
-            'status' => 'failure',
+        $request->validate([
+            'login' => 'required|string',
+            'password' => 'required|string',
         ]);
+
+        try {
+            $token = $this->authService->login($request->login, $request->password);
+            return response()->json([
+                'status' => 'success',
+                'token' => $token,
+            ], 200);
+        } catch (\Exception $e) {
+            return response()->json(['status' => 'failure'], 401);
+        }
     }
 }
